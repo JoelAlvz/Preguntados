e3a6472082a74a99363d7535e887070772acd92b
Joel Álvarez Rodriguez <joel@joel>
2024-11-06 19:22:14 +0100

    Commit creado interfaz del servicio y servicio

--- src/main/java/com/app/preguntados/model/dto/dtomapper/PreguntaMapper.java
+++ src/main/java/com/app/preguntados/model/dto/dtomapper/PreguntaMapper.java
@@ -2,10 +2,11 @@ package com.app.preguntados.model.dto.dtomapper;
 
 import com.app.preguntados.model.Pregunta;
 import com.app.preguntados.model.dto.PreguntaDTO;
+import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
-
+@Mapper
 public interface PreguntaMapper {
     PreguntaMapper INSTANCE = Mappers.getMapper(PreguntaMapper.class);
     PreguntaDTO toDTO(Pregunta pregunta);
